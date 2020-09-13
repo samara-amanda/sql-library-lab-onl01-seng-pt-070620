7230aee99acc5a7d9e64eab655e853a99b1f6d55
Samara Grant <samara.amanda01@gmail.com>
2020-09-13 19:38:21 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -22,6 +22,16 @@ CREATE TABLE books (
   series_id INTEGER
 );
 
-CREATE TABLE (
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER
+);
 
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  book_id INTEGER,
+  character_id INTEGER
 );
